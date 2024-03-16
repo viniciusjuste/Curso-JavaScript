b86ef20373facedce210b44976e8d2e81fd1caac
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2024-03-15 22:57:01 -0300

    Criei os arquivos da aula71

---
+++ aula71(calculadora-P2)/aula71.js
@@ -0,0 +1,25 @@
+const teclasNum = [...document.querySelectorAll(".num")] 
+const teclasOp =  [...document.querySelectorAll(".op")]
+const teclasRes = document.querySelector(".res")
+const display = document.querySelector(".display")
+const ton = document.getElementById("ton")
+const tlimpar = document.getElementById("tlimpar")
+
+teclasNum.forEach((el) => {
+    el.addEventListener('click', (evt) => {
+        display.innerHTML += evt.target.innerHTML
+    })
+})
+
+teclasOp.forEach((el) => {
+    el.addEventListener('click', (evt) => {
+        display.innerHTML += evt.target.innerHTML
+    })
+})
+
+tlimpar.addEventListener('click', (evt) => {
+    display.innerHTML = '0'
+})
+
+console.log(teclasNum)
+console.log(teclasOp)
