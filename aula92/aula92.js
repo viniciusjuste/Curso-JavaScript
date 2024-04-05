651686cd826a2ca8c34d4e694f0a40741605e085
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2024-04-05 18:11:15 -0300

    Atualizei os arquivos da aula92

---
+++ aula92/aula92.js
@@ -0,0 +1,12 @@
+
+import { cursos } from "./cursos.js" // Aqui eu importo o que eu quero usar.
+import { carros } from "./cursos.js";
+
+cursos.forEach(element => {
+    console.log(element)
+});
+
+carros.forEach(element => {
+    console.log(element)
+});
+
---
+++ aula92/cursos.js
@@ -0,0 +1,4 @@
+const cursos = ['JavaScript', 'HTMl','CSS', 'Arduino', 'Raspberry', 'C++', 'Python', 'Java', 'C#']
+const carros = ['Polo', 'T-Cross', 'Golf', 'Onix', 'Cruze', 'Argo']
+
+export {cursos, carros} // Tenho que exportar o que eu quero pegar.
