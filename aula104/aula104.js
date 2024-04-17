6d793f36caab7c1f12d0043c08c76414b23b5e37
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2024-04-17 17:36:41 -0300

    Criei a aula104

---
+++ aula104/aula104.js
@@ -0,0 +1,18 @@
+let nome = new String('Bruno Pinho Campos')
+
+// O método startsWith() retorna true se a string 'nome' começa com 'Pinho', caso contrário, retorna false.
+console.log(nome.startsWith('Bruno'))
+
+// O método endsWith() retorna true se a string 'nome' termina com 'Pinho', caso contrário, retorna false.
+console.log(nome.endsWith('Pinho'))
+
+// O método includes() retorna true se a string 'nome' contém 'Pinho', caso contrário, retorna false.
+console.log(nome.includes('Pinho'))
+
+// O método repeat(2) retorna uma nova string contendo a repetição da string 'nome' duas vezes.
+console.log(nome.repeat(2))
+
+// O método String.fromCodePoint(66) retorna o caractere correspondente ao código Unicode especificado, que neste caso é 'B'.
+console.log(String.fromCodePoint(66))
+
+
