9555e49d378301c28219d3963623270897d08027
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2024-04-23 19:02:34 -0300

    Criei os arquivos da aula114

---
+++ aula114/aula114.js
@@ -0,0 +1,9 @@
+let numeros = [10, 20, 30, 40]
+
+let [a, b, c, d] = numeros
+// ({a, b, c, d} = {a: 'Verde', b: 'Amarelo', c: 'Azul', d: 'Branco'})
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
