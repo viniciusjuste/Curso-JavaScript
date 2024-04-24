00579b73bc765423deb4bf0da293845f1069114a
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2024-04-24 18:27:45 -0300

    Criei a aula118

---
+++ aula118/aula118.js
@@ -0,0 +1,27 @@
+// Casos em que o ';' é obrigatório.
+
+/*
+let i = null
+for(i = 0; i < 10; i++);
+    console.log(i)
+----------------------------------------------
+let i = null; i = 0
+console.log(i)
+----------------------------------------------
+let canal = 'CFB Cursos';
+['a,b,c,d,f,r,g'].forEach(l => console.log(l));
+----------------------------------------------
+let n1 = [1, 2]
+let n2 = [3, 4];
+[n1, n2].map(v => console.log(v))
+----------------------------------------------
+let numeros = [10, 20, 30, 40];
+[n1, n2, n3] = numeros
+console.log(n1)
+*/
+
+
+
+
+
+
