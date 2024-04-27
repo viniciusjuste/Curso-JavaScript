a1ac4c1f43452e9fec7abc5390170fc3fcfa0309
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2024-04-26 22:21:37 -0300

    Adicionei um projeto

---
+++ advice-slip-API/advice.js
@@ -0,0 +1,24 @@
+const p_advice = document.getElementById('p_advice');
+const btn_advice = document.getElementById('btn_advice');
+
+const obterConselho = () => {
+    const originalAdviceEndpoint = 'https://api.adviceslip.com/advice';
+    const translateEndpoint = 'https://api.mymemory.translated.net/get?q=';
+    
+    fetch(originalAdviceEndpoint)
+    .then(response => response.json())
+    .then(data => {
+        const originalAdvice = data.slip.advice;
+        const encodedAdvice = encodeURIComponent(originalAdvice);
+        const languagePair = 'en|pt'; // Inglês para Português
+
+        return fetch(translateEndpoint + encodedAdvice + '&langpair=' + languagePair);
+    })
+    .then(response => response.json())
+    .then(translationData => {
+        const translatedAdvice = translationData.matches[0].translation;
+        p_advice.innerHTML = translatedAdvice;
+    });
+};
+
+btn_advice.addEventListener('click', obterConselho);
