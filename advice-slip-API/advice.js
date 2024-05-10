38b3a3b2339d96dbea1aa3942cb3e5418cbf9f30
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2024-05-10 18:46:37 -0300

    Melhorei a identação

--- advice-slip-API/advice.js
+++ advice-slip-API/advice.js
@@ -4,21 +4,21 @@ const btn_advice = document.getElementById('btn_advice');
 const obterConselho = () => {
     const originalAdviceEndpoint = 'https://api.adviceslip.com/advice';
     const translateEndpoint = 'https://api.mymemory.translated.net/get?q=';
-    
-    fetch(originalAdviceEndpoint)
-    .then(response => response.json())
-    .then(data => {
-        const originalAdvice = data.slip.advice;
-        const encodedAdvice = encodeURIComponent(originalAdvice);
-        const languagePair = 'en|pt'; // Inglês para Português
 
-        return fetch(translateEndpoint + encodedAdvice + '&langpair=' + languagePair);
-    })
-    .then(response => response.json())
-    .then(translationData => {
-        const translatedAdvice = translationData.matches[0].translation;
-        p_advice.innerHTML = translatedAdvice;
-    });
+    fetch(originalAdviceEndpoint)
+        .then(response => response.json())
+        .then(data => {
+            const originalAdvice = data.slip.advice;
+            const encodedAdvice = encodeURIComponent(originalAdvice);
+            const languagePair = 'en|pt'; // Inglês para Português
+
+            return fetch(translateEndpoint + encodedAdvice + '&langpair=' + languagePair);
+        })
+        .then(response => response.json())
+        .then(translationData => {
+            const translatedAdvice = translationData.matches[0].translation;
+            p_advice.innerHTML = translatedAdvice;
+        });
 };
 
 btn_advice.addEventListener('click', obterConselho);
