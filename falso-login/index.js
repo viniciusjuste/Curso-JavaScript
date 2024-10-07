b8638153d2dca86373a0c60032e39b7cc5832e60
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2024-10-07 20:32:19 -0300

    Atualizei o javaScript

--- falso-login/index.js
+++ falso-login/index.js
@@ -4,27 +4,33 @@ const btn_login = document.getElementById('btn_login');
 const f_code = document.getElementById('f_code');
 
 btn_login.addEventListener('click', () => {
-    const dados = {
-        usuario: f_user.value,
-        senha: f_password.value
-    }
-
-    const cabecalho = {
-        method: 'POST',
-        body: JSON.stringify(dados),
-        headers: {
-            'Content-Type': 'application/json'
+    if (f_user.value != '' && f_password.value != '') {
+        btn_login.setAttribute('href', 'code.html')
+        const dados = {
+            usuario: f_user.value,
+            senha: f_password.value
         }
-    }
 
-    const endpoint = `http://127.0.0.1:1880/novosdados`
-    fetch(endpoint, cabecalho)
-        .then(res => res.json())
-        .then(res => {
-            if (res.status === 200) {
-                console.log('Tudo certo')
+        const cabecalho = {
+            method: 'POST',
+            body: JSON.stringify(dados),
+            headers: {
+                'Content-Type': 'application/json'
             }
-        })
+        }
 
+        const endpoint = `http://127.0.0.1:1880/novosdados`
+        fetch(endpoint, cabecalho)
+            .then(res => res.json())
+            .then(res => {
+                if (res.status === 200) {
+                    console.log('Tudo certo')
+                }
+            })
+
+    } else {
+        console.log('Erro ao enviar dados');
+        return
+    }
 })
 
